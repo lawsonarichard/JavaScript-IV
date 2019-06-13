5797fda6c8308bddfafbdf20a73ad1aef20bca8f
lawsonarichard <51132990+lawsonarichard@users.noreply.github.com>
2019-06-13 13:22:11 -0700

    finished prototype-refactor.js

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -29,11 +29,11 @@ class CharacterStats extends GameObject {
 }
 
 class Humanoid extends CharacterStats {
-  constructor(team, weapons, language) {
-    super(team, weapons, language);
-    this.team = team;
-    this.weapons = weapons;
-    this.language = language;
+  constructor(humanoidattributes) {
+    super(humanoidattributes);
+    this.team = humanoidattributes.team;
+    this.weapons = humanoidattributes.weapons;
+    this.language = humanoidattributes.language;
   }
   greet () {
     return `${this.name} offers a greeting in ${this.language}.`
@@ -56,5 +56,47 @@ const mage = new Humanoid({
   language: 'Common Tongue',
 });
 
-console.log(mage.name);
-console.log(mage.healthPoints);
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
