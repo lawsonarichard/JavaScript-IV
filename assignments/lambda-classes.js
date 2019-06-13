1610d5d9d782c379a817e50e3e0e13754a5ae763
lawsonarichard <51132990+lawsonarichard@users.noreply.github.com>
2019-06-13 15:17:23 -0700

    finished lambda-class.js and attempted strech see line 33

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,7 +6,7 @@ class Person {
         this.location = personclass.location;
     }
     speak () {
-        console.log (`Hello my name is ${this.name}, I am from ${this.location}.`)
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
     };
 }
 
@@ -17,11 +17,11 @@ class Instructor extends Person {
         this.favLanguage = instructorclass.favLanguage;
         this.catchPhrase = instructorclass.catchPhrase;
     }
-    demo () {
-        console.log (`Today we are learning about ${subject}`)
+    demo (subject) {
+        return `Today we are learning about ${subject}`
     };
-    grade () {
-        console.log (`${student.name} recieves a perfect score on ${this.subject}.`)
+    grade (student, subject) {
+        return `${student.name} recieves a perfect score on ${subject}.`
     };
 }
 class Student extends Person {
@@ -30,16 +30,20 @@ class Student extends Person {
         this.previousBackground = studentclass.previousBackground;
         this.className = studentclass.className;
         this.favSubjects = studentclass.favSubjects;
-
+        this.grade = function getRandom (min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max-min + 1)) + min;
+        };
     }
     listsSubjects () {
-        console.log (`Here are my fav subjects: ${this.favSubjects}.`)
+        return `Here are my fav subjects: ${this.favSubjects}.`
     };
-    PRAssignment () {
-        console.log (`${this.name} has submitted a PR for ${this.subject}.`)
+    PRAssignment (subject) {
+        return `${this.name} has submitted a PR for ${subject}.`
     };
-    sprintChallenge () {
-        console.log ( `${this.name} has begun sprint challenge on ${this.subject}.`)
+    sprintChallenge (student, subject) {
+        return `${student.name} has begun sprint challenge on ${this.subject}.`
     };
 }
 
@@ -49,11 +53,11 @@ class ProjectManager extends Instructor {
         this.gradClassname = projectmanagerclass.gradClassname;
         this.favInstructor = projectmanagerclass.favInstructor;
     }
-    standUp () {
-        console.log (`${this.name} announces to ${this.channel}, @channel standy times!`)
+    standUp (name, channel) {
+        return `${this.name} announces to ${this.channel}, @channel standy times!`
     };
-    debugsCode () {
-        console.log (`${this.name} debugs ${this.student.name}'s code on ${this.subject}.`)
+    debugsCode (student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}.`
     };
 }
 
@@ -77,8 +81,15 @@ const Billy = new Student ({
     catchPhrase: 'Cowabunga Dudes',
     favSubjects: 'Cats, sushi, pizza',
     previousBackground: 'Guitarist',
-    
 });
 
-console.log(Fred.speak);
-console.log(Billy.PRAssignment);
+const George = new ProjectManager ({
+    gradClassname: 'CS1',
+    favInstructor: 'Sean',
+    name: 'George',
+});
+
+console.log(Fred.speak());
+console.log(Billy.PRAssignment('bouncy ball'));
+console.log(George.gradClassname);
+console.log(George.debugsCode(Billy, 'cats'))
